7157e12d4bfaf0bc938e59b5d97588cea810bfa9
vinay878787 <vinay1112002@gmail.com>
2025-05-29 23:40:45 +0530

    refactor: implemented express middleware instead of standaloneserver

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,46 @@
 import "dotenv/config";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema/type-def.js";
 import { resolvers } from "./schema/resolvers.js";
 import { verifyJwt } from "./utils/helper-functions/auth.js";
+import cors from "cors";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+import express from "express";
+import http from "http";
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req }) => {
-    const authHeader = req.headers.authorization || "";
-    let user = null;
-    if (authHeader.startsWith("Bearer ")) {
-      const token = authHeader.replace("Bearer ", "");
-      user = verifyJwt(token);
-    }
-    return { user };
-  },
-});
+await server.start();
 
-console.log(` Server ready at: ${url}`);
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  }),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+      const authHeader = req.headers.authorization || "";
+      let user = null;
+      if (authHeader.startsWith("Bearer ")) {
+        const token = authHeader.replace("Bearer ", "");
+        user = verifyJwt(token);
+      }
+      return { user };
+    },
+  })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+console.log(`Server ready at http://localhost:4000/graphql`);
--- server/utils/helper-functions/auth.ts
+++ server/utils/helper-functions/auth.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export function signJwt(payload: object) {
   const JWT_SECRET = process.env.JWT_SECRET;
   if (!JWT_SECRET) throw new Error("JWT_SECRET is not set in environment variables.");
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET);
 }
 
 export function verifyJwt(token: string) {
